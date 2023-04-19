60cf53e66738e37b41c218253ee77e11bc2d783c
lcavuoti <lcavuoti@clip.ch>
2023-04-19 21:45:16 +0200

    App.test.js changed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import useFetch from "./services/useFetch";
-import {getBusinessTrips} from "./services/tripsService";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+//import useFetch from "./services/useFetch";
+//import {getBusinessTrips} from "./services/tripsService";
 
 //
 // test('the fetch fails with an error', async () => {
@@ -18,15 +18,14 @@ import {getBusinessTrips} from "./services/tripsService";
 // });
 
 //----
-  const sum = function sum(a, b) {
-    return a + b;
-  }
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+const sum = function sum(a, b) {
+  return a + b;
+};
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
 
 //----
-  test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
-  });
-
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
