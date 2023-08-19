8b9678298b4bf8f8b4e7e5863721b0800362f275
lcavuoti <lcavuoti@clip.ch>
2023-08-19 17:30:43 +0200

    test-pilpeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import {logRoles, render, screen} from "@testing-library/react";
 import App from "./App";
 //import useFetch from "./services/useFetch";
 //import {getBusinessTrips} from "./services/tripsService";
@@ -8,11 +8,18 @@ it("ret without crashing", () => {
 
 it("renders Account header", () => {
   const wrapper = shallow(<App />);
-  const welcome = <h1>DWelcome to biztrips</h1>;
+  const welcome = <h1>Welcome to biztrips</h1>;
   expect(wrapper.contains(welcome)).toEqual(true);
 });
 
+test('App renders a heading', () => {
+  render(<App />)
 
+  screen.getByRole('heading', {
+    name: "Welcome to biztrips",
+  })
+
+});
 
 describe("SearchForm", () => {
   test("renders SearchForm", () => {
